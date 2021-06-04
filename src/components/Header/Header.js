aefa64b9730c6361f1dd2bf280397b052dc9e4eb
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-04 23:14:20 +0300

    Create Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,16 @@
+import styles from './Header.module.css';
 
 const Header = () => {
-    return <div>Hello!</div>
+    return (
+        <div className={styles.container}>
+            Movies
+
+            <div>
+                <a href="/login" className={styles.linkButton}>Login</a>
+                <a href="/register" className={styles.linkButton}>Register</a>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
