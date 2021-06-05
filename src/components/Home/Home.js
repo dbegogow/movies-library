a84855a2ee37a8ed59e387932a25fe7f7dccebfb
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-06 02:01:20 +0300

    Fixed movie article styles and add two movies in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,21 +7,16 @@ const Home = () => {
             <h2 className={styles.title}>All movies</h2>
 
             <div className={styles.articlesContainer}>
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
-                <Article />
+                <Article
+                    title="Home alone"
+                    image="https://images-na.ssl-images-amazon.com/images/I/A1T%2BlZ6iUZL._SL1500_.jpg"
+                    description="An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation."
+                />
+                <Article
+                    title="Home alone 2"
+                    image="https://ctycms.com/md-pike-district/images/99440320.jpg"
+                    description="The film follows Kevin (Culkin), a 10-year-old boy, who once again must fend off two burglars, Harry and Marv (Pesci and Stern), after he is mistakenly separated from his family on their Christmas vacation."
+                />
             </div>
         </div>
     );
