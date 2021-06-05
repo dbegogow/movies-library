8b3fbef6860f00115f6c39c5e8f7ae908de5ae0d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-05 11:38:03 +0300

    Add Login Component and background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import Home from './components/Home/Home';
+import Login from './components/Login';
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Home />
+            <Login />
         </div>
     );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import styles from './Login.module.css';
+
+const Login = () => {
+    return (
+        <section className={styles.container}>
+                
+        </section>
+    );
+};
+
+export default Login;
