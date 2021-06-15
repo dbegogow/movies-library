9ca23cb3a2d6042775a244feae949389a2d9ae84
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-15 13:04:02 +0300

    Fixed Article component styles

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,8 +1,10 @@
 import styles from './Movie.module.css';
 
-const Movie = () => {
+const Movie = (props) => {
     return (
-        <div className={styles.container}>Movie</div>
+        <div>
+            <img src="https://m.media-amazon.com/images/M/MV5BMzFkM2YwOTQtYzk2Mi00N2VlLWE3NTItN2YwNDg1YmY0ZDNmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" alt="" />
+        </div>
     );
 };
 
