b30e9774a761db41794096a4b171318c07bb5a3d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-15 13:58:07 +0300

    Add styles in Movie component

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react';
 import styles from './Movie.module.css';
 
 const Movie = (props) => {
+    const [id, setId] = useState('');
+
+    useEffect(() => {
+        
+    }, []);
+
     return (
-        <div>
-            <img src="https://m.media-amazon.com/images/M/MV5BMzFkM2YwOTQtYzk2Mi00N2VlLWE3NTItN2YwNDg1YmY0ZDNmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" alt="" />
+        <div className={styles.container}>
+            <img className={styles.image}
+                src="https://m.media-amazon.com/images/M/MV5BMzFkM2YwOTQtYzk2Mi00N2VlLWE3NTItN2YwNDg1YmY0ZDNmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+                alt=""
+            />
+            <div className={styles.descriptionContainer}>
+                <h1 className={styles.title}>Home Alone</h1>
+                <div className={styles.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod quas beatae architecto nemo suscipit dolores iusto libero numquam voluptate molestiae laborum distinctio recusandae velit, deleniti ducimus itaque? Tempora, optio adipisci?</div>
+            </div>
         </div>
     );
 };
